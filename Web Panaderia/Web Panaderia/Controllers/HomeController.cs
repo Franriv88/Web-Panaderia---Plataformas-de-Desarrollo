a5a35a97079a297d78e8e017242b420faf4c04a1
Francisco Rivas <francisco.rivas@davinci.edu.ar>
2023-06-29 01:10:50 -0300

    Aspecto del login completo

--- Web Panaderia/Web Panaderia/Controllers/HomeController.cs
+++ Web Panaderia/Web Panaderia/Controllers/HomeController.cs
@@ -13,19 +13,7 @@ namespace Web_Panaderia.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
          public ActionResult Login()
         {
             ViewBag.Message = "Your login page.";
